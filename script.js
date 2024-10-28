ff3a5228012f47b969ff412f6493cf2def9f57fd
faellim <166923971+faellim@users.noreply.github.com>
2024-10-28 17:15:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-js const request = async () => {
-    const reqResult = await fetch("https://hltv-api.vercel.app/api/news.json");
-    const result = await reqResult.json();
-    console.log(result);
-  };
-  
-  request();
+
